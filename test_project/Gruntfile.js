201a9612f2d0b1629fd4ec72ad847c8b74e86fba
jsudufour <jsudufour@gmail.com>
2015-09-07 20:22:56 -0400

    added example files to test grunt and node/npm package installations.

---
+++ test_project/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', ['watch']);
+
+    grunt.registerTask('fun', 'This task is for fun only', function() {
+    	grunt.log.writeln('This is the *fun* Grunt task!');
+    });
+
+    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
+    	grunt.log.writeln('Wipe that smirk off your face; this is serious.');
+    });
+
+    grunt.initConfig({
+    	watch: {
+    		files: ['Gruntfile.js']
+    	},
+
+    	copy: {
+    		main: {
+    			files: {
+					'./dest/': ['./js/*.js'],
+				}
+			}
+    	}
+    }); 
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    
+};
